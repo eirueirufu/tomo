5db6357c5b231bc2b0515df70516d27ee21314e4
eirueirufu <731358689@qq.com>
2023-08-06 12:57:14 +0800

    feat

---
+++ app/assistant-setting/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+  Input,
+  Textarea,
+  Button,
+} from "@nextui-org/react";
+import { PaperPlaneTilt, UploadSimple } from "@phosphor-icons/react";
+
+export default function Page() {
+  return (
+    <Card className="w-full h-full">
+      <CardHeader className="flex gap-3 flex-col sm:flex-row">
+        <Image
+          alt="nextui logo"
+          radius="sm"
+          src="/user.jpeg"
+          width={48}
+          height={48}
+        />
+        <Input size="sm" type="text" label="Name" defaultValue="伊知地虹夏" />
+        <Input
+          size="sm"
+          type="text"
+          label="Description"
+          defaultValue="下北沢の大天使"
+        />
+      </CardHeader>
+      <Divider />
+      <CardBody className="flex-1">
+        <Textarea
+          placeholder="メッセージをインプットください"
+          minRows={10}
+          maxRows={99}
+          defaultValue="バンドのメンバーです！"
+        />
+      </CardBody>
+      <Divider />
+      <CardFooter className="flex flex-row-reverse">
+        <Button
+          size="sm"
+          type="submit"
+          radius="full"
+          className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+        >
+          <UploadSimple size={24} weight="bold" />
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -12,41 +12,62 @@ import {
 import vhCheck from "vh-check";
 import { useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   useEffect(() => {
     vhCheck();
   }, []);
 
   return (
     <div className="w-full h-full flex flex-col gap-1 items-center p-3">
-      <Link href="/chat" className="w-full">
-        <Card isHoverable isPressable className="w-full">
-          <CardBody className="flex flex-row p-2">
-            <User
-              name="伊知地虹夏"
-              description="下北沢の大天使"
-              avatarProps={{
-                src: "/user.jpeg",
-              }}
-            />
-          </CardBody>
-        </Card>
-      </Link>
+      <Card
+        isHoverable
+        isPressable
+        className="w-full"
+        onClick={() => {
+          router.push("/chat");
+        }}
+      >
+        <CardBody className="flex flex-row p-2">
+          <User
+            name="伊知地虹夏"
+            description="下北沢の大天使"
+            avatarProps={{
+              src: "/user.jpeg",
+            }}
+            onClick={(e) => {
+              e.stopPropagation();
+              router.push("/assistant-setting");
+            }}
+          />
+        </CardBody>
+      </Card>
 
-      <Link href="/chat" className="w-full">
-        <Card isHoverable isPressable className="w-full">
-          <CardBody className="flex flex-row p-2">
-            <User
-              name="伊知地虹夏"
-              description="下北沢の大天使"
-              avatarProps={{
-                src: "/user.jpeg",
-              }}
-            />
-          </CardBody>
-        </Card>
-      </Link>
+      <Card
+        isHoverable
+        isPressable
+        className="w-full"
+        onClick={() => {
+          router.push("/chat");
+        }}
+      >
+        <CardBody className="flex flex-row p-2">
+          <User
+            name="伊知地虹夏"
+            description="下北沢の大天使"
+            avatarProps={{
+              src: "/user.jpeg",
+            }}
+            onClick={(e) => {
+              e.stopPropagation();
+              router.push("/assistant-setting");
+            }}
+          />
+        </CardBody>
+      </Card>
     </div>
   );
 }
