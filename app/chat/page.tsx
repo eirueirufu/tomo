cb2035d36ef8bef710e623626a03aabe4a36789a
eirueirufu <731358689@qq.com>
2023-08-06 17:19:02 +0800

    feat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -4,22 +4,36 @@ import { Textarea, Button, Spinner } from "@nextui-org/react";
 import MsgAssistant from "@/components/msg-assistant";
 import MsgUser from "@/components/msg-user";
 import { PaperPlaneTilt } from "@phosphor-icons/react";
-import { useChat } from "ai/react";
+import { useChat, Message } from "ai/react";
 import vhCheck from "vh-check";
 import { useEffect } from "react";
 
-export default function Home() {
+export default function Page(props: { preMessages?: Message[] }) {
   useEffect(() => {
     vhCheck();
   }, []);
 
-  const { messages, input, handleInputChange, handleSubmit, stop, isLoading } =
-    useChat();
+  const {
+    messages,
+    setMessages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    stop,
+    isLoading,
+  } = useChat();
+
+  if (props.preMessages) {
+    setMessages(props.preMessages);
+  }
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
       <div className="w-full flex-1 flex flex-col-reverse gap-2 overflow-y-auto">
         {[...messages].reverse().map((message) => {
+          if (!message.id) {
+            return;
+          }
           switch (message.role) {
             case "assistant":
               return <MsgAssistant key={message.id} msg={message.content} />;
