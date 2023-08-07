49bdb91902f3924b0b6aaac7d39c6c36d4ff1dd8
eirueirufu <731358689@qq.com>
2023-08-07 23:20:23 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   const [assistants, setAssistants] = useState<WithId<Assistant>[]>([]);
   useEffect(() => {
     (async () => {
-      const response = await fetch("/api/assistants");
+      const response = await fetch("/api/assistants", { cache: "no-cache" });
       const assistants: WithId<Assistant>[] = await response.json();
       setAssistants(assistants);
     })();
