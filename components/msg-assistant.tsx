aefa9046268cb28a6dc8cd08eea882ef4b234e5e
eirueirufu <731358689@qq.com>
2023-08-08 22:29:31 +0800

    fix

--- components/msg-assistant.tsx
+++ components/msg-assistant.tsx
@@ -1,13 +1,11 @@
 import { Avatar } from "@nextui-org/react";
 
 export default function MsgAssistant(props: { avatar?: string; msg: string }) {
-  const { avatar = "/user.jpeg", msg } = props;
-
   return (
     <div className="w-full flex flex-nowrap p-2">
-      <Avatar src="/user.jpeg" />
+      <Avatar src={props.avatar} />
       <div className="ml-2 max-w-[70%] bg-slate-600 rounded-xl bottom-1 mt-1 p-2">
-        {msg}
+        {props.msg}
       </div>
     </div>
   );
