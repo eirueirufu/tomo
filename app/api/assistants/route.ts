e9cbb9f55a29433bc1d88e67c8cd3838945b1612
eirueirufu <731358689@qq.com>
2023-08-08 20:42:09 +0800

    fix

--- app/api/assistants/route.ts
+++ app/api/assistants/route.ts
@@ -1,8 +1,10 @@
 import clientPromise from "@/lib/mongodb";
 import { Assistant } from "@/models/assistants";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
+  cookies();
   const client = await clientPromise;
   const collection = client.db("gpt").collection<Assistant>("assistants");
   const cursor = collection.find();
