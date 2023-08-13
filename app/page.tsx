0daaf1d8ec6c889241398446234988edb3474063
eirueirufu <731358689@qq.com>
2023-08-13 15:54:49 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
           </NavbarItem>
         </NavbarContent>
       </Navbar>
-      <div className="container m-auto flex-1 flex flex-col gap-2 items-center p-3">
+      <div className="container m-auto flex-1 flex flex-col gap-2 items-center p-3 overflow-y-auto">
         {assistants.map((assistant) => {
           return (
             <Card
