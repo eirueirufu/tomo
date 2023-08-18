e4f9e216bd8ba6398d409aaf54cdbbec04ac23a8
eirueirufu <731358689@qq.com>
2023-08-18 21:50:22 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,8 @@
 
 import {
   Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
   Avatar,
   Button,
-  User,
   Navbar,
   NavbarContent,
   NavbarItem,
@@ -15,12 +11,10 @@ import {
 } from "@nextui-org/react";
 
 import { useEffect, useState } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import vhCheck from "vh-check";
 import { Assistant } from "@/models/assistant";
 import { WithId } from "mongodb";
-import { PlusCircle } from "@phosphor-icons/react";
 import Loading from "@/components/loading";
 import { ThemeSwitcher } from "@/components/themeSwitch";
 
@@ -29,7 +23,6 @@ export default function Home() {
     vhCheck();
   }, []);
 
-  const router = useRouter();
   const [assistants, setAssistants] = useState<WithId<Assistant>[]>();
   useEffect(() => {
     (async () => {
@@ -41,66 +34,85 @@ export default function Home() {
 
   return (
     <div className="h-[calc(100vh_-_var(--vh-offset,_0px))] m-auto flex flex-col">
-      <Navbar isBordered>
-        <NavbarContent justify="start"></NavbarContent>
-        <NavbarBrand>
-          <p className="font-bold m-auto">TOMO</p>
-        </NavbarBrand>
-        <NavbarContent justify="end">
-          <NavbarItem>
-            <ThemeSwitcher />
-          </NavbarItem>
-          <NavbarItem>
-            <Button
-              size="sm"
-              color="default"
-              variant="flat"
-              onClick={() => {
-                router.push(`/assistants/insert`);
-              }}
-            >
-              ADD
-            </Button>
-          </NavbarItem>
-        </NavbarContent>
-      </Navbar>
       <div className="container m-auto flex-1 flex flex-col gap-2 items-center p-3 overflow-y-auto">
-        {assistants ? (
-          assistants.map((assistant) => {
-            return (
-              <Card
-                key={assistant._id.toString()}
-                isHoverable
-                isPressable
-                className="w-full p-3 overflow-visible"
-                onClick={() => {
-                  router.push(`/chat/${assistant._id.toString()}`);
-                }}
-              >
-                <div className="flex flex-row">
-                  <Avatar
-                    src={assistant.avatar}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      router.push(
-                        `/assistants/update/${assistant._id.toString()}`,
-                      );
-                    }}
-                  />
-                  <div className="text-start ml-2">
-                    <p className="text-sm">{assistant.name}</p>
-                    <p className="text-xs text-zinc-500">
-                      {assistant.description}
-                    </p>
-                  </div>
-                </div>
-              </Card>
-            );
-          })
-        ) : (
-          <Loading />
-        )}
+        <Nav />
+        <ChatList assistants={assistants} />
       </div>
     </div>
   );
+}
+
+export function Nav() {
+  const router = useRouter();
+  return (
+    <Navbar isBordered>
+      <NavbarContent justify="start"></NavbarContent>
+      <NavbarBrand>
+        <p className="font-bold m-auto">TOMO</p>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <ThemeSwitcher />
+        </NavbarItem>
+        <NavbarItem>
+          <Button
+            size="sm"
+            color="default"
+            variant="flat"
+            onClick={() => {
+              router.push(`/assistants/insert`);
+            }}
+          >
+            ADD
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
+
+export function ChatList(props: {
+  assistants: WithId<Assistant>[] | undefined;
+}): JSX.Element {
+  const router = useRouter();
+
+  if (props.assistants) {
+    return (
+      <>
+        {props.assistants.map((assistant) => {
+          return (
+            <Card
+              key={assistant._id.toString()}
+              isHoverable
+              isPressable
+              className="w-full p-3 overflow-visible"
+              onClick={() => {
+                router.push(`/chat/${assistant._id.toString()}`);
+              }}
+            >
+              <div className="flex flex-row">
+                <Avatar
+                  src={assistant.avatar}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    router.push(
+                      `/assistants/update/${assistant._id.toString()}`,
+                    );
+                  }}
+                />
+                <div className="text-start ml-2">
+                  <p className="text-sm">{assistant.name}</p>
+                  <p className="text-xs text-zinc-500">
+                    {assistant.description}
+                  </p>
+                </div>
+              </div>
+            </Card>
+          );
+        })}
+      </>
+    );
+  } else {
+    return <Loading />;
+  }
 }
