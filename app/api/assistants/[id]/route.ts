13071829d0eeacd19877521b504606ab964b393a
eirueirufu <731358689@qq.com>
2023-08-19 18:44:20 +0800

    fix

--- app/api/assistants/[id]/route.ts
+++ app/api/assistants/[id]/route.ts
@@ -4,18 +4,6 @@ import { NextRequest, NextResponse } from 'next/server';
 import { WithId, ObjectId } from 'mongodb';
 import { Message } from '@/models/message';
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } },
-) {
-  const client = await clientPromise;
-  const collection = client
-    .db('gpt')
-    .collection<WithId<Assistant>>('assistants');
-  const assistant = await collection.findOne({ _id: new ObjectId(params.id) });
-  return NextResponse.json(assistant);
-}
-
 export async function POST(
   request: NextRequest,
   { params }: { params: { id: string } },
