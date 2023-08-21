7fe7c31e643e824c24935a095d6457088f3b7842
eirueirufu <731358689@qq.com>
2023-08-21 21:04:06 +0800

    fix: dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import HomeAssistants from '@/components/homeAssistants';
 import HomeNav from '@/components/homeNav';
 import { View } from '@/components/view';
+import { cookies } from 'next/headers';
 
 export default async function Page() {
+  cookies();
   return (
     <View className="flex flex-col">
       <HomeNav />
